7802cb0cd9229df49e5a1dbc6563285e11de3e36
ba <brandon.ardoin@selu.edu>
2020-11-17 21:01:17 -0600

    Added ability to push rocks into holes

---
+++ The First of Us/Assets/Scripts/RockInHole.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockInHole : MonoBehaviour
+{
+    GameObject rock;
+    GameObject hole;
+    GameObject filledhole;
+    //private SpriteRenderer sprite;
+
+    void Start () {
+        //sprite = GetComponent<SpriteRenderer>();
+        filledhole = GameObject.FindWithTag ("FilledHole");
+        filledhole.SetActive (false);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Rocky"))
+        {
+            Debug.Log("Rock over hole");
+            rock = GameObject.FindWithTag ("Rocky");
+            rock.SetActive (false);
+            hole = GameObject.FindWithTag ("EmptyHole");
+            hole.SetActive (false);
+            //filledhole = GameObject.FindWithTag ("FilledHole");
+            //filledhole.sortingOrder = 4;
+            filledhole.SetActive (true);
+
+        }
+    }
+
+}
