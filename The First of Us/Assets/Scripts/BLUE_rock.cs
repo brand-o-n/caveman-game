7021f3bec88dbb9cb04cf2cb389ab649079f57ae
ba <brandon.ardoin@selu.edu>
2020-11-17 23:11:09 -0600

    puzzle 1

---
+++ The First of Us/Assets/Scripts/BLUE_rock.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BLUE_rock : MonoBehaviour
+{
+    GameObject brock;
+    GameObject bhole;
+    GameObject bfilledhole;
+
+    void Start () {
+        bfilledhole = GameObject.FindWithTag ("FilledHole_BLUE");
+        bfilledhole.SetActive (false);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("BLUE_rock"))
+        {
+            Debug.Log("Rock over hole");
+            brock = GameObject.FindWithTag ("BLUE_rock");
+            brock.SetActive (false);
+            bhole = GameObject.FindWithTag ("EmptyHole_BLUE");
+            bhole.SetActive (false);
+            bfilledhole.SetActive (true);
+
+        }
+    }
+
+}
---
+++ The First of Us/Assets/Scripts/RED_rock.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RED_rock : MonoBehaviour
+{
+    GameObject rrock;
+    GameObject rhole;
+    GameObject rfilledhole;
+
+    void Start () {
+        rfilledhole = GameObject.FindWithTag ("FilledHole_RED");
+        rfilledhole.SetActive (false);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("RED_rock"))
+        {
+            Debug.Log("Rock over hole");
+            rrock = GameObject.FindWithTag ("RED_rock");
+            rrock.SetActive (false);
+            rhole = GameObject.FindWithTag ("EmptyHole_RED");
+            rhole.SetActive (false);
+            rfilledhole.SetActive (true);
+
+        }
+    }
+
+}
